48d0dd49ff272b8f67876450669620613ad0ca01
Samuel Fabián <sfabian1996@gmail.com>
2021-11-26 16:58:33 -0300

    Implementação botão e cores

--- piwm/views/IMCApp.jsx
+++ piwm/views/IMCApp.jsx
@@ -1,34 +1,47 @@
 import React, { Component } from "react"
-import { View, Text, TextInput, Image } from 'react-native'
+import { View, Text, TextInput, Button } from 'react-native'
 import IMCCAlc from "./IMCCalc"
 
 export default class IMCApp extends Component {
     constructor(props) {
         super(props)
-        this.state = { altura: null, peso: null }
+        this.state = { altura: null, peso: null, click: false }
     }
+    
+    clickBotao = () =>{
+        if(this.state.altura != null && this.state.peso != null){
+            this.setState({click:true})
+        }
+    }
+
+    mostrarIMC(){
+        if(this.state.click)
+            return (
+                <IMCCAlc altura={this.state.altura} peso={this.state.peso}/>
+            )
+    }
+
     render() {
         return (
             <View>
                 <Text style={{ fontSize: 25, fontWeight: 'bold', marginBottom: 10 }}>IMC CALCULATOR</Text>
                 
-                <Text>Sua altura:</Text>
+                <Text>Sua altura (m):</Text>
                 
                 <TextInput 
                 onChangeText={(altura) => this.setState({altura:altura})} 
-                placeholder="Altura em M" 
+                placeholder="Exemplo: 1.80" 
                 style={{ marginTop: 10, marginBottom: 10, backgroundColor: 'white' }}/>
                 
-                <Text>Seu peso:</Text>
+                <Text>Seu peso (kg):</Text>
                 
                 <TextInput 
                 onChangeText={(peso) => this.setState({peso:peso})} 
-                placeholder="Peso em Kg" 
+                placeholder="Exemplo: 80" 
                 style={{ marginTop: 10, marginBottom: 10, backgroundColor: 'white' }}/>
                 
-                <button style={{ width: 100, marginBottom: 10, alignSelf: 'center' }}>CALCULAR</button>
-                
-                <IMCCAlc altura={this.state.altura} peso={this.state.peso}/>
+                <Button title="CALCULAR" onPress={this.clickBotao} style={{ width: 100, marginBottom: 10, alignSelf: 'center' }}/>
+                {this.mostrarIMC()}
             </View>
         )
     }
--- piwm/views/IMCMsg.jsx
+++ piwm/views/IMCMsg.jsx
@@ -2,13 +2,13 @@ import React from "react"
 import { Text } from 'react-native'
 
 const message = (props)=>{
-    if(props.IMC<17) return <Text style={{fontSize:20}}>Muito baixo do peso!!!</Text>
-    if(props.IMC<=18.49) return <Text style={{fontSize:20}}>Abaixo do peso!!!</Text>
-    if(props.IMC<=24.99) return <Text style={{fontSize:20}}>Peso normal.</Text>
-    if(props.IMC<=29.99) return <Text style={{fontSize:20}}>Acima do peso!!!</Text>
-    if(props.IMC<=34.99) return <Text style={{fontSize:20}}>Obesidade I</Text>
-    if(props.IMC<=39.99) return <Text style={{fontSize:20}}>Obesidade II (severa)</Text>
-    return <Text style={{fontSize:20}}>Obesidade III (mórbida)</Text>
+    if(props.IMC<17) return <Text style={{fontSize:20, color: 'red', fontWeight: 'bold'}}>Muito baixo do peso!!!</Text>
+    if(props.IMC<=18.49) return <Text style={{fontSize:20, color: 'red', fontWeight: 'bold'}}>Abaixo do peso!!!</Text>
+    if(props.IMC<=24.99) return <Text style={{fontSize:20, color:'blue', fontWeight: 'bold'}}>Peso normal.</Text>
+    if(props.IMC<=29.99) return <Text style={{fontSize:20, color: 'red', fontWeight: 'bold'}}>Acima do peso!!!</Text>
+    if(props.IMC<=34.99) return <Text style={{fontSize:20, color: 'red', fontWeight: 'bold'}}>Obesidade I</Text>
+    if(props.IMC<=39.99) return <Text style={{fontSize:20, color: 'red', fontWeight: 'bold'}}>Obesidade II (severa)</Text>
+    return <Text style={{fontSize:20, color: 'red', fontWeight: 'bold'}}>Obesidade III (mórbida)</Text>
 }
 
 export default message
